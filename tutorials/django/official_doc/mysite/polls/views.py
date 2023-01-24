324d14c63f09d404be5db4bad4e33497320f4c1f
slifrin <jurek.kiedrowski@gmail.com>
2023-01-24 21:22:12 +0100

    Add more testing and some changes in logic

--- tutorials/django/official_doc/mysite/polls/views.py
+++ tutorials/django/official_doc/mysite/polls/views.py
@@ -3,39 +3,50 @@ from django.http import HttpRequest, HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Question, Choice
 
+
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DeleteView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DeleteView):
     model = Question
-    template_name = 'polls/results.html'
+    template_name = "polls/results.html"
 
 
 def vote(request: HttpRequest, question_id):
     question: Question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice: Choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice: Choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You dind't select a choice.",
-        })
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You dind't select a choice.",
+            },
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        print(reverse('polls:results', args=(question.id,)))
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        print(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
