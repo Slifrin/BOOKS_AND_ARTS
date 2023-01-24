7688e14feffc385625e55d8180df2093461dfa49
slifrin <jurek.kiedrowski@gmail.com>
2023-01-24 16:43:10 +0100

    Update missing variable name

--- tutorials/django/official_doc/mysite/polls/views.py
+++ tutorials/django/official_doc/mysite/polls/views.py
@@ -9,7 +9,7 @@ from polls.models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = ''
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
