e1f5b7255bc997aedc48df081e2594822feaf4b4
slifrin <jurek.kiedrowski@gmail.com>
2023-01-25 09:24:31 +0100

    Customization of admin page

--- tutorials/django/official_doc/mysite/polls/admin.py
+++ tutorials/django/official_doc/mysite/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline): # Or use StackedInline which takes more space
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
