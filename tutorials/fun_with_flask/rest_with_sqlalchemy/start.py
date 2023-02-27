5abf9f2a69f1f217d672ac3868142072892a89b9
slifrin <jurek.kiedrowski@gmail.com>
2023-02-27 17:01:51 +0100

    Add usage of SQLAlchemy and Marshmallow and change usage of connexion

--- tutorials/fun_with_flask/rest_with_sqlalchemy/start.py
+++ tutorials/fun_with_flask/rest_with_sqlalchemy/start.py
@@ -3,13 +3,15 @@ from flask import render_template
 import connexion
 
 import config
+from models import Person
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8000, debug=True)
